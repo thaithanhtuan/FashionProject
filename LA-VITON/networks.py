4714aa22dd13ef5504bc20db60fb1253b53d4b91
Thái Thanh Tuấn <thaithanhtuan1987@yahoo.com>
2020-02-14 15:33:18 +0900

    Add DT1 and DT2 for 1 norm and 2 norm distance of gic loss

--- LA-VITON/networks.py
+++ LA-VITON/networks.py
@@ -405,11 +405,50 @@ class DT(nn.Module):
     def __init__(self):
         super(DT, self).__init__()
 
+    def forward(self,x1, x2):
+        dt = torch.abs(x1 - x2)
+        return dt
+
+class DT2(nn.Module):
+    def __init__(self):
+        super(DT, self).__init__()
+
     def forward(self,x1, y1, x2, y2):
         dt = torch.sqrt(torch.mul(x1 - x2, x1 - x2) + torch.mul(y1 - y2, y1 - y2))
         return dt
-
+#for 1 norm distance
 class GicLoss(nn.Module):
+    def __init__(self,opt):
+        super(GicLoss, self).__init__()
+        self.dT = DT()
+        self.opt = opt
+
+
+
+    def forward(self, grid):
+        Gx = grid[:, :, :, 0]
+        Gy = grid[:, :, :, 1]
+        Gxcenter = Gx[:, 1:self.opt.fine_height - 1, 1:self.opt.fine_width - 1]
+        Gxup = Gx[:, 0:self.opt.fine_height - 2, 1:self.opt.fine_width - 1]
+        Gxdown = Gx[:, 2:self.opt.fine_height, 1:self.opt.fine_width - 1]
+        Gxleft = Gx[:, 1:self.opt.fine_height - 1, 0:self.opt.fine_width - 2]
+        Gxright = Gx[:, 1:self.opt.fine_height - 1, 2:self.opt.fine_width]
+
+        Gycenter = Gy[:, 1:self.opt.fine_height - 1, 1:self.opt.fine_width - 1]
+        Gyup = Gy[:, 0:self.opt.fine_height - 2, 1:self.opt.fine_width - 1]
+        Gydown = Gy[:, 2:self.opt.fine_height, 1:self.opt.fine_width - 1]
+        Gyleft = Gy[:, 1:self.opt.fine_height - 1, 0:self.opt.fine_width - 2]
+        Gyright = Gy[:, 1:self.opt.fine_height - 1, 2:self.opt.fine_width]
+
+        dtleft = self.dT(Gxleft, Gxcenter)
+        dtright = self.dT(Gxright, Gxcenter)
+        dtup = self.dT(Gyup, Gycenter)
+        dtdown = self.dT(Gydown, Gycenter)
+
+        return torch.sum(torch.abs(dtleft - dtright) + torch.abs(dtup - dtdown))
+
+#For 2norm distance
+class GicLoss2(nn.Module):
     def __init__(self,opt):
         super(GicLoss, self).__init__()
         self.dT = DT()
@@ -438,7 +477,6 @@ class GicLoss(nn.Module):
         dtdown = self.dT(Gxdown, Gydown, Gxcenter, Gycenter)
 
         return torch.sum(torch.abs(dtleft - dtright) + torch.abs(dtup - dtdown))
-
 
 
 class GMM(nn.Module):
